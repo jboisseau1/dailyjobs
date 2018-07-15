8f523b827a9d390dda6ae8869b8d35784c743017
Jacob Boisseau <jacobboisseau@gmail.com>
2018-07-15 15:40:54 -0400

    added calls for everyday of the work week

--- dailyjob.py
+++ dailyjob.py
@@ -1,11 +1,18 @@
 import schedule
 import time
+import requests
 
 def job():
-    print("I'm working...")
+    print("Time to make a call to port 3005")
+    requests.get("http://localhost:3005")
+
     return
 
-schedule.every().day.at("20:47").do(job)
+schedule.every().monday.at("15:30").do(job)
+schedule.every().tuesday.at("15:30").do(job)
+schedule.every().wednesday.at("15:30").do(job)
+schedule.every().thursday.at("15:30").do(job)
+schedule.every().friday.at("15:30").do(job)
 
 
 while True:
